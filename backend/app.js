53d4c0be303095a04ae3b2a0c1122945d8c44e3d
zabaoit <nguyengiabao8407@gmail.com>
2025-10-04 09:54:39 +0700

    setup & connect mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/vi/status", (req, res) => {
+  res.status(200).json({ status: "Api  is running ..." });
 });
 
-app.listen(3000, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export default app;
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import app from "./app.js";
+
+dotenv.config({ path: "./.config.env" });
+
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
+
+// MongoDB connection
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("DB connection  successful");
+  })
+  .catch(err => {
+    console.log("DB connection error: ", err.message);
+  });
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
