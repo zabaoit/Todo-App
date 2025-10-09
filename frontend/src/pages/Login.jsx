904c80eb3e379effefee302cf1e7fe8d241a003a
zabaoit <nguyengiabao8407@gmail.com>
2025-10-09 10:47:12 +0700

    design login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,6 @@ const Login = ({ onLoginSuccess }) => {
         return;
       }
 
-      // ✅ Lưu token vào localStorage
       localStorage.setItem("token", data.token);
 
       setMessage("Đăng nhập thành công!");
@@ -33,31 +32,58 @@ const Login = ({ onLoginSuccess }) => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
-      <form onSubmit={handleLogin} className="bg-gray-800 p-8 rounded-2xl shadow-lg w-full max-w-sm">
-        <h2 className="text-2xl font-semibold mb-6 text-center">Đăng nhập</h2>
+    <div class="bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 flex min-h-screen flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 class="mt-10 text-center text-3xl font-bold  text-white">Sign in to your account</h2>
+      </div>
 
-        <input
-          type="email"
-          placeholder="Email"
-          className="w-full p-3 mb-4 rounded bg-gray-700 outline-none"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Mật khẩu"
-          className="w-full p-3 mb-4 rounded bg-gray-700 outline-none"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form onSubmit={handleLogin} class="space-y-6">
+          <div>
+            <label class="block text-xl font-medium text-gray-100">Email</label>
+            <div class="mt-2">
+              <input
+                type="email"
+                value={email}
+                autoComplete="username"
+                onChange={e => setEmail(e.target.value)}
+                class="block w-full bg-white/5 px-3 py-1.5 text-base text-white border border-gray-600 rounded-lg placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 sm:text-sm/6 "
+              />
+            </div>
+          </div>
 
-        <button type="submit" className="w-full bg-teal-600 py-2 rounded hover:bg-teal-700 transition">
-          Đăng nhập
-        </button>
+          <div>
+            <div class="flex items-center justify-between">
+              <label class="block text-xl font-medium text-gray-100">Password</label>
+              <div class="text-md">
+                <a href="#" class="font-semibold text-gray-400 hover:text-gray-300">
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+            <div class="mt-2">
+              <input
+                type="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                class="block w-full bg-white/5 px-3 py-1.5 text-base text-white border border-gray-600 rounded-lg placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-500 sm:text-sm/6"
+              />
+            </div>
+          </div>
 
-        {message && <p className="mt-4 text-center text-sm">{message}</p>}
-      </form>
+          <div className="flex justify-center max-w-60 mx-auto">
+            <button
+              type="submit"
+              class="flex w-full justify-center rounded-lg bg-gray-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-gray-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500 cursor-pointer"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+
+        {message && <p class="mt-10 text-center text-md text-gray-400">{message}</p>}
+      </div>
     </div>
   );
 };
